34443b657bf88efc560d950a5331852482a69386
RhyanO2 <rhyanoliveirads@gmail.com>
2025-10-03 22:32:38 -0300

    API changes

--- src/Services/api.jsx
+++ src/Services/api.jsx
@@ -5,31 +5,40 @@ const api = axios.create({
 });
 
 //Usuario
-export const getUsuario = () => api.get(`/usuario`);
-export const getUsuarioById = (id) => api.get(`/usuario/${id}`);
-export const createUsuario = (data) => api.post(`/usuario/insert`,data);
-export const updateUsuario = (id,data) => api.put(`/usuario/insert/${id}`,data);
-export const deleteUsuario = (id,data) => api.delete(`/usuario/insert/${id}`,data);
+export const getUsuarios = () => api.get(`/usuarios`);
+export const getUsuariosById = (id) => api.get(`/usuarios/${id}`);
+export const createUsuarios = (data) => api.post(`/usuarios/insert`,data);
+export const updateUsuarios = (id,data) => api.put(`/usuarios/insert/${id}`,data);
+export const deleteUsuarios = (id) => api.delete(`/usuarios/del/${id}`);
 
 //Agendamento
-export const getAgendamento = () => api.get(`/agendamento`);
-export const getAgendamentoById = (id) => api.get(`/agendamento/${id}`);
-export const createAgendamento = (data) => api.post(`/agendamento/insert`,data);
-export const updateAgendamento = (id,data) => api.put(`/agendamento/insert/${id}`,data);
-export const deleteAgendamento = (id,data) => api.delete(`/agendamento/insert/${id}`,data);
+export const getAgendamentos = () => api.get(`/agendamentos`);
+export const getAgendamentosById = (id) => api.get(`/agendamentos/${id}`);
+export const createAgendamentos = (data) => api.post(`/agendamentos/insert`,data);
+export const updateAgendamentos = (id,data) => api.put(`/agendamentos/insert/${id}`,data);
+export const deleteAgendamentos = (id) => api.delete(`/agendamentos/del/${id}`);
 
 //Atendimento
-export const getAtendimento = () => api.get(`/atendimento`);
-export const getAtendimentoById = (id) => api.get(`/atendimento/${id}`);
-export const createAtendimento = (data) => api.post(`/atendimento/insert`,data);
-export const updateAtendimento = (id,data) => api.put(`/atendimento/insert/${id}`,data);
-export const deleteAtendimento = (id,data) => api.delete(`/atendimento/insert/${id}`,data);
+export const getAtendimentos = () => api.get(`/atendimentos`);
+export const getAtendimentosById = (id) => api.get(`/atendimentos/${id}`);
+export const createAtendimentos = (data) => api.post(`/atendimentos/insert`,data);
+export const updateAtendimentos = (id,data) => api.put(`/atendimentos/insert/${id}`,data);
+export const deleteAtendimentos = (id) => api.delete(`/atendimentos/del/${id}`);
 
 //Consulta
-export const getConsulta = () => api.get(`/consulta`);
-export const getConsultaById = (id) => api.get(`/consulta/${id}`);
-export const createConsulta = (data) => api.post(`/consulta/insert`,data);
-export const updateConsulta = (id,data) => api.put(`/consulta/insert/${id}`,data);
-export const deleteConsulta = (id,data) => api.delete(`/consulta/insert/${id}`,data);
+export const getConsultas = () => api.get(`/consultas`);
+export const getConsultasById = (id) => api.get(`/consultas/${id}`);
+export const createConsultas = (data) => api.post(`/consultas/insert`,data);
+export const updateConsultas = (id,data) => api.put(`/consultas/insert/${id}`,data);
+export const deleteConsultas = (id) => api.delete(`/consultas/del/${id}`);
 
+
+export const getReceitas = () => api.get(`/receitas`);
+export const getReceitasById = (id) => api.get(`/receitas/${id}`);
+export const createReceitas = (data) => api.post(`/receitas/insert`,data);
+export const updateReceitas = (id,data) => api.put(`/receitas/insert/${id}`,data);
+export const deleteReceitas = (id) => api.delete(`/receitas/del/${id}`);
+
+//status
 export const getConsultaByStatus = (status) => api.get(`/status/${status}`)
+export const getConsultaBydate = (data) => api.get(`/status/date/${data}`)
